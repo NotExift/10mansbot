7f96772966f1d89e18468a86077c940ab7b92a26
Cooper <coopermw03@gmail.com>
2024-05-15 14:43:22 -0500

    Format

--- bot/game_logic.py
+++ bot/game_logic.py
@@ -50,7 +50,7 @@ class playerButton(Button):
         self.player = player
 
     async def callback(self, interaction):
-        global current_cap, player_button_menu, player_picks_embed
+        global current_cap, player_buttons, player_button_menu, player_picks_embed
         await interaction.response.defer(ephemeral=True)
 
         if interaction.user == current_cap:
@@ -62,28 +62,13 @@ class playerButton(Button):
                 current_cap = init.TEAM1_CAP
 
             # Remove this button from the view
+            player_buttons.remove(self)
             player_button_menu.remove_item(self)
-
-            player_picks_embed.clear_fields()
-            player_picks_embed.add_field(
-                name="👥 Team 1",
-                value="\n".join([f"<@{user.id}>" for user in init.TEAM1]),
-                inline=True,
-            )
-            player_picks_embed.add_field(
-                name="👥 Team 2",
-                value="\n".join([f"<@{user.id}>" for user in init.TEAM2]),
-                inline=True,
-            )
 
             await interaction.followup.send(
                 content=f"You have selected {self.player}!", ephemeral=True
             )
-            await interaction.message.edit(
-                content=f"{current_cap.mention} please select a player!",
-                embed=player_picks_embed,
-                view=player_button_menu,
-            )
+            await update_player_picks_message(interaction.message)
         else:
             await interaction.followup.send(
                 "It is either not your turn, or you are not allowed to make a selection.",
@@ -92,15 +77,18 @@ class playerButton(Button):
 
 
 async def pick_players():
-    global current_cap, player_button_menu, player_picks_embed
+    global current_cap, player_buttons, player_button_menu, player_picks_embed
     init.TEAM1 = [init.TEAM1_CAP]
     init.TEAM2 = [init.TEAM2_CAP]
     current_cap = init.TEAM1_CAP
 
     player_button_menu = View()
+    player_buttons = []
     for player in init.QUEUE:
         if player != init.TEAM1_CAP and player != init.TEAM2_CAP:
-            player_button_menu.add_item(playerButton(player))
+            button = playerButton(player)
+            player_buttons.append(button)
+            player_button_menu.add_item(button)
 
     player_picks_embed = discord.Embed(title="Player Picks", color=0x00FF00)
     player_picks_embed.add_field(
@@ -120,12 +108,52 @@ async def pick_players():
         view=player_button_menu,
     )
 
-    while len(init.TEAM1) < init.TEAM_SIZE or len(init.TEAM2) < init.TEAM_SIZE:
-        await asyncio.sleep(1)
+    while len(init.TEAM1) < init.TEAM_SIZE or len(init.TEAM2) < init.TEAM_SIZE - 1:
+        try:
+            await asyncio.wait_for(
+                wait_for_captain_decision(current_cap), timeout=init.VETO_WINDOW
+            )
+        except asyncio.TimeoutError:
+            # Timeout occurred, automatically ban a category
+            button = (
+                player_buttons.pop()
+            )  # Remove last item, we can change this later if needed
+            player_button_menu.remove_item(button)
+            if current_cap == init.TEAM1_CAP:
+                init.TEAM1.append(button.player)
+                current_cap = init.TEAM2_CAP
+            else:
+                init.TEAM2.append(button.player)
+                current_cap = init.TEAM1_CAP
+            await update_player_picks_message(picks_msg)
+
+    init.TEAM2.append(player_buttons[0].player)
+    await update_player_picks_message(picks_msg)
 
     await picks_msg.edit(content=None, embed=player_picks_embed, view=None)
 
     return init.TEAM1, init.TEAM2
+
+
+async def update_player_picks_message(message):
+    global player_button_menu, player_picks_embed
+    player_picks_embed.clear_fields()
+    player_picks_embed.add_field(
+        name="👥 Team 1",
+        value="\n".join([f"<@{user.id}>" for user in init.TEAM1]),
+        inline=True,
+    )
+    player_picks_embed.add_field(
+        name="👥 Team 2",
+        value="\n".join([f"<@{user.id}>" for user in init.TEAM2]),
+        inline=True,
+    )
+
+    await message.edit(
+        content=f"{current_cap.mention} please select a player!",
+        embed=player_picks_embed,
+        view=player_button_menu,
+    )
 
 
 """ ================================================== MAP/CATEGORY BANS ================================================== """
@@ -137,7 +165,7 @@ class categoryButton(Button):
         self.category_name = category_name
 
     async def callback(self, interaction):
-        global current_cap, category_button_menu, category_embed
+        global current_cap, category_buttons, category_button_menu, category_embed
         await interaction.response.defer(ephemeral=True)
 
         if interaction.user == current_cap:
@@ -147,26 +175,13 @@ class categoryButton(Button):
                 current_cap = init.TEAM1_CAP
 
             init.CATEGORIES.remove(self.category_name)
-
+            category_buttons.remove(self)
             category_button_menu.remove_item(self)
-
-            category_embed.clear_fields()
-            for category in init.CATEGORIES:
-                category_embed.add_field(
-                    name=category,
-                    value="\n".join(map_name for map_name in init.MAPS[category]),
-                    inline=True,
-                )
-            category_embed.set_image(url="attachment://bot/mapsimage.jpg")
 
             await interaction.followup.send(
                 content=f"You have removed {self.category_name}!", ephemeral=True
             )
-            await interaction.message.edit(
-                content=f"{current_cap.mention} please ban a category!",
-                embed=category_embed,
-                view=category_button_menu,
-            )
+            await update_category_veto_message(interaction.message)
         else:
             await interaction.followup.send(
                 "It is either not your turn, or you are not allowed to make a selection.",
@@ -180,7 +195,7 @@ class mapButton(Button):
         self.map_name = map_name
 
     async def callback(self, interaction):
-        global current_cap, map_list, map_button_menu, map_embed
+        global current_cap, map_list, map_buttons, map_button_menu, map_embed
         await interaction.response.defer(ephemeral=True)
 
         if interaction.user == current_cap:
@@ -190,24 +205,13 @@ class mapButton(Button):
                 current_cap = init.TEAM1_CAP
 
             map_list.remove(self.map_name)
+            map_buttons.remove(self)
             map_button_menu.remove_item(self)
-
-            map_embed.clear_fields()
-            map_embed.add_field(
-                name=init.CATEGORIES[0],
-                value="\n".join(map_name for map_name in map_list),
-                inline=True,
-            )
-            map_embed.set_image(url="attachment://bot/mapsimage.jpg")
 
             await interaction.followup.send(
                 content=f"You have removed {self.map_name}!", ephemeral=True
             )
-            await interaction.message.edit(
-                content=f"{current_cap.mention} please ban a map!",
-                embed=map_embed,
-                view=map_button_menu,
-            )
+            await update_map_veto_message(interaction.message)
         else:
             await interaction.followup.send(
                 "It is either not your turn, or you are not allowed to make a selection.",
@@ -260,13 +264,16 @@ class endgameButton(Button):
 
 
 async def start_map_ban(ctx):
-    global current_cap, map_list, category_button_menu, category_embed, map_button_menu, map_embed
+    global current_cap, map_list, category_buttons, map_buttons, category_button_menu, category_embed, map_button_menu, map_embed
 
     init.set_map_config()  # every time a new game is started the list resets
 
     category_button_menu = View()
+    category_buttons = []
     for category in init.CATEGORIES:
-        category_button_menu.add_item(categoryButton(category))
+        button = categoryButton(category)
+        category_buttons.append(button)
+        category_button_menu.add_item(button)
 
     category_embed = discord.Embed(title="Category Veto", color=0x00FF00)
     for category in init.CATEGORIES:
@@ -275,25 +282,42 @@ async def start_map_ban(ctx):
             value="\n".join(map_name for map_name in init.MAPS[category]),
             inline=True,
         )
-    mfile = discord.File(f"bot/mapsimage.jpg", filename=f"mapsimage.jpg")
     category_embed.set_image(url="attachment://mapsimage.jpg")
 
     veto_msg = await init.MATCHROOM_CHANNEL.send(
         content=f"{current_cap.mention}, please ban a category!",
+        file=discord.File(f"bot/mapsimage.jpg", filename=f"mapsimage.jpg"),
         embed=category_embed,
         view=category_button_menu,
-        file=mfile,
     )
+
     while len(init.CATEGORIES) > 1:
         if not init.GAME_ONGOING:
             return
-        await asyncio.sleep(1)
+        # Wait for captain to make a decision within 20 seconds
+        try:
+            await asyncio.wait_for(
+                wait_for_captain_decision(current_cap), timeout=init.VETO_WINDOW
+            )
+        except asyncio.TimeoutError:
+            # Timeout occurred, automatically ban a category
+            init.CATEGORIES.pop()  # Remove last item, we can change this later if needed
+            button = category_buttons.pop()
+            category_button_menu.remove_item(button)
+            if current_cap == init.TEAM1_CAP:
+                current_cap = init.TEAM2_CAP
+            else:
+                current_cap = init.TEAM1_CAP
+            await update_category_veto_message(veto_msg)
 
     map_list = list(init.MAPS.options(init.CATEGORIES[0]))
 
     map_button_menu = View()
+    map_buttons = []
     for map_name in map_list:
-        map_button_menu.add_item(mapButton(map_name))
+        button = mapButton(map_name)
+        map_buttons.append(button)
+        map_button_menu.add_item(button)
 
     map_embed = discord.Embed(title="Map Veto", color=0x00FF00)
     map_embed.add_field(
@@ -306,22 +330,43 @@ async def start_map_ban(ctx):
         embed=map_embed,
         view=map_button_menu,
     )
+
     while len(map_list) > 1:
         if not init.GAME_ONGOING:
             return
-        await asyncio.sleep(1)
+        # Wait for captain to make a decision within 20 seconds
+        try:
+            await asyncio.wait_for(
+                wait_for_captain_decision(current_cap), timeout=init.VETO_WINDOW
+            )
+        except asyncio.TimeoutError:
+            # Timeout occurred, automatically ban a category
+            map_list.pop()  # Remove last item, we can change this later if needed
+            button = map_buttons.pop()
+            map_button_menu.remove_item(button)
+            if current_cap == init.TEAM1_CAP:
+                current_cap = init.TEAM2_CAP
+            else:
+                current_cap = init.TEAM1_CAP
+            await update_map_veto_message(veto_msg)
 
     await veto_msg.edit(content=None, embed=map_embed, view=None)
 
+    # Create a valve rcon connection to the counterstrike server
+    await init.MATCHROOM_CHANNEL.send(
+        "Please wait for your match server to to be created!"
+    )
+    await change_map(map_list[0])
+
     if init.GAME_ONGOING:
         current_date_time = datetime.datetime.now().strftime("%B %d, %Y, %H:%M:%S")
-        button = Button(
+        server_button = Button(
             label="Click to Join Server",
             style=discord.ButtonStyle.url,
             url="http://connect.exift.gay/",
         )
-        view = View()
-        view.add_item(button)
+        view = View(timeout=None)
+        view.add_item(server_button)
         view.add_item(copyIPButton())
         view.add_item(endgameButton())
         # Create the embed message
@@ -336,9 +381,6 @@ async def start_map_ban(ctx):
             name="👥 Team 2",
             value="\n".join([f"<@{user.id}>" for user in init.TEAM2]),
             inline=True,
-        )
-        embed.set_footer(
-            text=f"connect {init.SERVER_IP}:{init.SERVER_PORT}; password okkkkkkk"
         )
         imageid = init.MAP_IDS.get(map_list[0])
         try:
@@ -357,13 +399,16 @@ async def start_map_ban(ctx):
             f"bot/thumbnail_cache/{imageid}.jpg", filename=f"{imageid}.jpg"
         )
         await init.MATCHROOM_CHANNEL.send(
-            content=f'The final map is: {map_list[0]}\n Reminder that one of the captains click "endgame" whenever the match concludes to reopen the queue!',
+            content=f'The final map is: **{map_list[0]}**\n Reminder that one of the captains need to click "endgame" whenever the match concludes to reopen the queue!',
             file=ifile_matchroom,
             embed=embed,
             view=view,
         )
 
         # Create a new embed message for the players
+        player_view = View()
+        player_view.add_item(server_button)
+        player_view.add_item(copyIPButton())
         player_embed = discord.Embed(
             title="Your game is ready! The server may take a minute before switching maps, please be patient.",
             color=0x00FF00,
@@ -378,12 +423,6 @@ async def start_map_ban(ctx):
             name="👥 Team 2",
             value="\n".join([f"<@{user.id}>" for user in init.TEAM2]),
             inline=True,
-        )
-        player_embed.set_footer(
-            text=f"connect {init.SERVER_IP}:{init.SERVER_PORT}; password okkkkkkk"
-        )
-        ifile = discord.File(
-            f"bot/thumbnail_cache/{imageid}.jpg", filename=f"{imageid}.jpg"
         )
         try:
             player_embed.set_thumbnail(url=f"attachment://{imageid}.jpg")
@@ -405,11 +444,11 @@ async def start_map_ban(ctx):
                 f"bot/thumbnail_cache/{imageid}.jpg", filename=f"{imageid}.jpg"
             )
             try:
-                await player.send(file=ifile_player, embed=player_embed, view=view)
+                await player.send(
+                    file=ifile_player, embed=player_embed, view=player_view
+                )
             except Exception as e:
                 print(f"Couldn't send message to {player.name}: {e}")
-        # Create a valve rcon connection to the counterstrike server
-    await change_map(map_list[0])
 
 
 async def change_map(final_map):
@@ -422,3 +461,49 @@ async def change_map(final_map):
             print("RCON commands executed successfully.")
     except Exception as e:
         print(f"Failed to execute RCON commands: {e}")
+
+
+async def wait_for_captain_decision(captain):
+    while True:
+        if current_cap != captain:
+            # Captain is no longer who we were waiting for, meaning they have selected
+            return
+        await asyncio.sleep(1)
+
+
+async def update_category_veto_message(message):
+    global category_button_menu, category_embed
+
+    category_embed.clear_fields()
+
+    for category in init.CATEGORIES:
+        category_embed.add_field(
+            name=category,
+            value="\n".join(map_name for map_name in init.MAPS[category]),
+            inline=True,
+        )
+    category_embed.set_image(url="attachment://bot/mapsimage.jpg")
+
+    await message.edit(
+        content=f"{current_cap.mention} please ban a category!",
+        embed=category_embed,
+        view=category_button_menu,
+    )
+
+
+async def update_map_veto_message(message):
+    global map_button_menu, map_embed
+
+    map_embed.clear_fields()
+    map_embed.add_field(
+        name=init.CATEGORIES[0],
+        value="\n".join(map_name for map_name in map_list),
+        inline=True,
+    )
+    map_embed.set_image(url="attachment://bot/mapsimage.jpg")
+
+    await message.edit(
+        content=f"{current_cap.mention} please ban a map!",
+        embed=map_embed,
+        view=map_button_menu,
+    )
